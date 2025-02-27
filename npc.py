85b1e9b6a91e014a9077bba237d6be7748f14261
Ayush Bist <retrobidness@gmail.com>
2025-02-27 19:25:22 +0545

    Update npc.py

--- npc.py
+++ npc.py
@@ -201,7 +201,7 @@ class CacoDemonNPC(NPC):
 
 class CyberDemonNPC(NPC):
     def __init__(self, game, path='resources/sprites/npc/cyber_demon/0.png', pos=(11.5, 6.0),
-                 scale=1.0, shift=0.04, animation_time=210):
+                scale=1.0, shift=0.04, animation_time=210):
         super().__init__(game, path, pos, scale, shift, animation_time)
         self.attack_dist = 6
         self.health = 350
